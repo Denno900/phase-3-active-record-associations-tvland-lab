9e83845d8b5345dbef296f2b1825d313632f7472
dennis mwaniki <dennomaz@gmail.com>
2023-02-26 23:00:56 +0300

    assignment complete

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+   has_many :characters
+   has_many :actors, through: :characters
+
+   def actors_list
+      actors_full_names = []
+      self.actors.map do |actor|
+         name = "#{actor.first_name} #{actor.last_name}"
+         actors_full_names << name
+      end
+
+      actors_full_names
+   end
 end
